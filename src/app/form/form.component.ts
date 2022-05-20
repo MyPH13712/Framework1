416588b3199651dfb93c0ca3ac965b55076bc7b2
Mydtph <79831056+MyPH13712@users.noreply.github.com>
2022-05-20 09:07:30 +0700

    CRUD

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -11,8 +12,42 @@ export class FormComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  onSubmit(formData: { name: string, age: number }) {
-    console.log(formData);
-
+  inputValues = {
+    name: '',
+    age: 0,
+    email: '',
+    sdt: 0
   }
+
+  users = [
+    {
+      id: 1,
+      name: 'mydt',
+      age: 20,
+      email: 'mydt@gmail.com',
+      sdt: 12345678
+    }
+
+  ];
+
+  onRemove(id: number) {
+    this.users = this.users.filter(item => item.id! === id);
+  }
+
+  onSubmit(userForm: NgForm) {
+    // console.log(formData);
+    const newUsersIds = this.users.map(user => user.id).sort((a: number, b: number) => b - a);
+    const maxId = newUsersIds[0]
+    this.users.push({
+      ...userForm.value,
+      id: maxId + 1
+    });
+    userForm.resetForm({
+      name: '',
+      age: 0,
+      email: '',
+      sdt: 0
+    })
+  }
+
 }
